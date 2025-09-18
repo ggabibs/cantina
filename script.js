4ba7c7531f1331de1164b6e35f7cae7533403e5f
ggabibs <maria.barbosa.sales@escola.pr.gov.br>
2025-09-18 11:39:23 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 var nomeCantina = "Cantina da Escola";
 console.log("Bem-vindo à " + nomeCantina);
-
 let salgados = 20;
 console.log("Temos " + salgados + " salgados disponíveis.");
-
 const precoSalgado = 5;
 console.log("Cada salgado custa R$" + precoSalgado);
-
 salgados = salgados - 5;
 let totalVendido = 5 * precoSalgado;
-
 console.log("Agora restam " + salgados + " salgados.");
 console.log("A cantina vendeu R$" + totalVendido);
 try {
@@ -17,14 +13,12 @@ try {
 } catch (erro) {
   console.log("Erro ao tentar alterar precoSalgado: " + erro.message);
 }
-
 if (true) {
   var testeVar = "Sou var";
   let testeLet = "Sou let";
   console.log(testeVar); 
   console.log(testeLet); 
 }
-
 console.log(testeVar); 
 try {
   console.log(testeLet); 
